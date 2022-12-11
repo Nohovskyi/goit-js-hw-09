f4dd9f88b5de4d177bec8054586e72dae920c66d
Nohovskyi <105544347+Nohovskyi@users.noreply.github.com>
2022-12-11 19:17:02 +0000

    Deploying to gh-pages from @ Nohovskyi/goit-js-hw-09@7f0b9fac36d89a37775f83d6625d5b80cf79f078 🚀

---
+++ 01-color-switcher.c738b30a.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.c738b30a.js.map
